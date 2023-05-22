b061beaee7c3387d1743d15f71013df6f87dc2a8
Sowjikate <sowjikate@gmail.com>
2023-05-22 09:05:34 +0530

    may13th assaignment1 utilizing Relational Operators

---
+++ may13thassaignment1.java
@@ -0,0 +1,29 @@
+// Question 1: The Relational Operators (==, !=, <=,>=)
+// Points: 5 Marks
+// Skill Mapping: Basic
+// Description - Consider two variables - num1 = 10;num2 = 20;
+// Write a Java program utilizing Relational Operators to produce the following output:
+// Equal: False
+// Not Equal: True
+// num1 is Greater Than num2: False
+// num1 is Less Than num2: True
+import java.util.Scanner;
+public class may13thassaignment1 {
+     public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+        boolean equal =(num1 == num2);
+        boolean notequal =(num1 != num2);
+        boolean isgreater=(num1> num2);
+        boolean isless=(num1<num2);
+           System.out.println("Equal:" +equal);
+           System.out.println("Not equal:" +notequal);
+           System.out.println("num1 is Greater than num2:" +isgreater);
+           System.out.println("num1 is less than num2:" +isless);
+     }}
+
+
+        
+    
+
+    
